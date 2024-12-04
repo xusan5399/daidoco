c992a6f30511c6238baecc3e3a492eadb71c037b
xusan5399 <118796449+xusan5399@users.noreply.github.com>
2024-12-04 06:31:09 +0000

    message

--- src/BasicFAB.js
+++ src/BasicFAB.js
@@ -5,7 +5,14 @@ import EditIcon from "@mui/icons-material/Edit";
 
 export default function FloatingActionButtons() {
   return (
-    <Box sx={{ "& > :not(style)": { m: 1 } }}>
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 16, // distance from the bottom of the screen
+        right: 16, // distance from the right side of the screen
+        "& > :not(style)": { m: 1 },
+      }}
+    >
       <Fab color="secondary" aria-label="edit">
         <EditIcon />
       </Fab>
